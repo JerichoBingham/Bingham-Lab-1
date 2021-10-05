d9b61fa8a1f7baeb3c4530d3ccf212fc98f2dcdd
jericho.bingham <jericho.bingham@OITW-COVID-LOAN>
2021-10-05 16:12:44 -0700

    Add project files.

---
+++ Bingham Lab 1/Bingham Lab 1.cpp
@@ -0,0 +1,29 @@
+//#include <iostream> 
+
+//int main()
+
+//{
+
+   // std::cout << "Modified version of original C program"
+
+   //     << "(by Kernighan & Ritchie) \n";
+
+   // std::cout << "Hello World!";
+
+   // return 0;
+
+//}
+
+#include <iostream> 
+
+int main()
+
+{
+
+    std::cout << "Jericho Bingham"
+
+        << "(Age 18)\n";
+
+        return 0;
+
+}
